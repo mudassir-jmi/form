ffa26f90ff260f3c0157504c3e8951accc03198b
Mudassir <64970467+mudassir-hash@users.noreply.github.com>
2020-05-08 17:30:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>form</title>
+  </head>
+  <body>
+  first name:<input type="text" name="first_name" value=""
+    password
+    <input type="password" name="password" value="">
+    gender:<input type="radio" name="gender">male
+    <input type="radio" name="gender">female
+    moblie number
+    <input type="text" name="mobilenumber" value="">
+  </body>
+</html>
